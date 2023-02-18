23d59764695971784a2f6b7391b53f9c65d2aa20
Alexandra Fontner <alex.fon03@gmail.com>
2023-02-18 18:14:34 +0100

    change title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,8 +3,12 @@ export default defineNuxtConfig({
   css: ["~/assets/style.css"],
   app: {
     head: {
+      title: "Alexandra Fontner",
       link: [
-        { rel: "stylesheet", href: "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" }
+        {
+          rel: "stylesheet",
+          href: "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css",
+        },
       ],
     },
   },
