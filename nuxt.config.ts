1f5f44323c0b199c911509d3761993b757c7af34
Alexandra Fontner <alex.fon03@gmail.com>
2023-02-18 18:19:54 +0100

    include icon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,12 +3,13 @@ export default defineNuxtConfig({
   css: ["~/assets/style.css"],
   app: {
     head: {
-      title: "Alexandra Fontner",
+      title: "Alexandra Fontner - Portfolio",
       link: [
         {
           rel: "stylesheet",
           href: "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css",
         },
+        { rel: "icon", href: "/favicon.ico" },
       ],
     },
   },
