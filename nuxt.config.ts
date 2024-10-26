65875420a4862da2734d3bffeffcce60f77333f8
Alexandra Fontner <alex.fon03@gmail.com>
2024-10-26 23:24:39 +0200

    Updated to nuxt 3.13.2

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   css: ["~/assets/style.css"],
+
   app: {
     head: {
       title: "Alexandra Fontner - Portfolio",
       link: [
         {
           rel: "stylesheet",
-          href: "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css",
+          href: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css",
         },
         {
           rel: "stylesheet",
@@ -16,4 +17,6 @@ export default defineNuxtConfig({
       ],
     },
   },
+
+  compatibilityDate: "2024-10-26",
 });
